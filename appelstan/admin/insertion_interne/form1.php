a26cec3cae02a0dd99d6a0580aeb6bdf197db884
AlexandreHebert77 <alexandre.2002.hebert@gmail.com>
2019-12-04 21:28:02 +0100

    Mise en forme boutton ilage

--- appelstan/admin/insertion_interne/form1.php
+++ appelstan/admin/insertion_interne/form1.php
@@ -1,46 +1,23 @@
-<!-- <?php
-// function connectMaBase(){
-//     $base = mysql_connect ('localhost', 'mamp', '');
-//     mysql_select_db ('ETUDE', $base) ;
-// }
-?> -->
+<!DOCTYPE html>
 <html>
-    <head><title>Formulaire de saisie d'un nouvel interne</title>
-<link rel="stylesheet" href="../../libs/bootstrap_sans_internet.css">
-<link rel="stylesheet" href="../../style/styleForm.css">
+	<head>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<h1>Ajouter un interne</h1>
+		<form method="post" action="form.php">
+			Nom: <label>Nom</label> <input type="text" name="Nom">
+			Prenom: <label>Prénom</label> <input type="text" name="Prenom">
+            <div class="classe">Classe : 	<input type="radio" name="classe" value="G"/>   2nde  <input type="radio" name="classe" value="F"/>   1ère  <input type="radio" name="classe" value="G"/>   Term  </div>
+			Place: <label>Place</label> <input type="email" name="Classe">
+            Image: <input name="userfile" type="file" />
+			<input type="submit" value="Ajouter">
 
-    </head>
-    <body>
-        <h1>Formulaire de saisie d'un nouvel interne</h1>
-        <!-- <div class="imgLogo"><img src="../../img/logoStanTransp.png" alt="logo"></div> -->
-        <h2>Entrez les données demandées :</h2>
+		<p>
+		<?php
+		if (isset($_POST['prenom'])) {
 
-
-
-<div class="formulaire">
-        <form name="Etude" method="post" action="form1.php">
-            Nom : <input class="form-control" type="text" name="Nom" placeholder="NOM"/> <br/>
-	         Prenom : <input class="form-control" type="text" name="Prenom" placeholder="Prénom"/> <br/>
-            <div class="classe" style="margin-bottom:12px;">Classe : 	<input type="radio" name="classe" value="G"/>   2nde  <input type="radio" name="classe" value="F"/>   1ère  <input type="radio" name="classe" value="G"/>   Term  </div>
-            Place : <input type="text" class="form-control" name="place" placeholder="Place"/><br/>
-            <!-- Image: <input name="userfile" type="file" accept=".png, .jpg, .jpeg, .svg, .mpeg"/>
-            <input type="submit" class="btn btn-lg" style="margin:10px; color:black;" name="valider" value="OK"/> -->
-            <div>
-              <label for="files" class="btn" style="background-color:#FFF; color:grey;">Selectionnez votre image</label>
-              <input id="files" style="visibility:hidden;" type="file">
-            </div>
-        </form>
-</div>
-        <?php
-
-        function connectMaBase(){
-            $base = mysql_connect ('localhost', 'mamp', '');
-            mysql_select_db ('ETUDE', $base) ;
-        }
-
-// ini_set('display_errors', 'on');
-
-        if (isset ($_POST['valider'])){
+            if (isset ($_POST['valider'])){
             $file = $_FILES['image']['tmp_name'];
 	        if (!isset($file))
 		    echo "S'il vous plait sélectionnez une image";
@@ -51,35 +28,22 @@
 	        $image_size = getimagesize($_FILES['image']['tmp_name']);
 
                 if ($image_size==FALSE)
-		    echo "Format de l'image invalide";
+		        echo "Format de l'image invalide";
                 else
                 {
-                    $Nom=$_POST['Nom'];
-                    $Prenom=$_POST['Prenom'];
-                    $Classe=$_POST['Classe'];
-	                  $Place=$_POST['Place'];
-                    //$Etat='ABS';
 
-                    //try { $bdd = new PDO('mysql:host=localhost;dbname=ETUDE','root',''); }
-                    //catch (Exeption $e) { die('Erreur : ' .$e->getMessage())  or die(print_r($bdd->errorInfo())); }
-                    //$req = $bdd->prepare("INSERT INTO appel VALUES(?,?,?,?,?)");
-                    //$req->execute(array($Nom,$Prenon,$Classe,$Place,$image));
-
-                    connectMaBase();
-                    //$sql = "INSERT INTO appel VALUES('$Nom','$Prenom','$Classe','$Place','$image')";
-                    $sql = 'INSERT INTO appel (Nom, Prenom, Classe, Place, Etat, Image) VALUES ("'.$Nom.'","'.$Prenom.'","'.$Classe.'","'.$Place.'","ABS","'.$image.'")';
-                    // connectMaBase();
-                    mysql_query ($sql) or die ('Erreur SQL !');
-
-            // on ferme la connexion
-                    mysql_close();
+			    $mysqli = new mysqli('localhost', 'root', 'root', 'ETUDE');
+			    $mysqli->set_charset('utf8');
+			    $requete='INSERT INTO appel VALUES("'.$_POST['Nom'].'","'.$_POST['Prenom'].'","'.$_POST['Classe'].'","'.$_POST['Place'].'",'ABS',"'.$image.'" )';
+			    $resultat = $mysqli->query($requete);
+			    if ($resultat)
+				    echo 'L interne a été ajouté';
+			    else
+				    echo 'Erreur';
                 }
-             }
-        }
-
-        ?>
-    </body>
+            }
+		}
+		?>
+		</p>
+	</body>
 </html>
-
-<!--
-(Nom, Prenom, Classe, Place, Etat, Image) -->
